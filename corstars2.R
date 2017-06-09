ab8e2328a2a4f15ce8e8d99ded92771ff688e160
lhbinge <lhbinge@gmail.com>
2017-06-09 17:34:16 +0200

    Add files via upload

---
+++ corstars2.R
@@ -0,0 +1,29 @@
+corstars2 <- function(x){ 
+    require(Hmisc) 
+    x <- as.matrix(x) 
+    R <- rcorr(x)$r 
+    p <- rcorr(x)$P 
+    
+    ## define notions for significance levels; spacing is important.
+    mystars <- ifelse(p < .001, "***", ifelse(p < .05, "** ", ifelse(p < .1, "* ", " ")))
+    
+    ## trunctuate the matrix that holds the correlations to two decimal
+    R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1] 
+    
+    ## build a new matrix that includes the correlations with their apropriate stars 
+    Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x))
+    Rnew <- ifelse(p < 0.05 & as.numeric(R)>0, paste0("\\colorbox{cyan}{", Rnew, "}"), Rnew)
+    
+    diag(Rnew) <- paste(diag(R), " ", sep="") 
+    rownames(Rnew) <- colnames(x) 
+    colnames(Rnew) <- paste(colnames(x), "", sep="") 
+    
+    ## remove upper triangle
+    Rnew <- as.matrix(Rnew)
+    Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew) 
+    
+    ## remove last column and return the matrix (which is now a data frame)
+    Rnew <- cbind(Rnew[1:length(Rnew)-1])
+    return(Rnew) 
+}
